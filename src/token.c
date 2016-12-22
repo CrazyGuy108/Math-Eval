e1bd239a6b059651c62acc88ef120b18a3e61598
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-22 15:09:42 -0800

    Create Token class
    The Token class represents a single immutable lexer token. Implemented
    using a struct, you aren't meant to directly access the fields inside it.
    Instead, use the functions defined below it.
    
    There's also an enum associated with one of the Token members, called
    TokenType. This defines what type of Token is represented.
    
    A TokenType to string converter was also made, which is helpful for debug or error messages within the lexer.
    
    The TokenType_punctuator function is like TokenType_toString but it does the opposite, giving you the correct TokenType based on the input.

--- src/token.c
+++ src/token.c
@@ -1,1 +1,73 @@
 #include "token.h"
+
+void
+Token_init(struct Token* t, enum TokenType type, const char* begin, const char* end)
+{
+	t->type = type;
+	t->begin = begin;
+	t->end = end;
+}
+
+enum TokenType
+Token_getType(struct Token* t)
+{
+	return t->type;
+}
+
+const char*
+Token_getBegin(struct Token* t)
+{
+	return t->begin;
+}
+
+const char*
+Token_getEnd(struct Token* t)
+{
+	return t->end;
+}
+
+const char*
+TokenType_toString(enum TokenType type)
+{
+	switch (type)
+	{
+	case TOKEN_LPAREN:   return "token_lparen";
+	case TOKEN_RPAREN:   return "token_rparen";
+	case TOKEN_COMMA:    return "token_comma";
+	case TOKEN_ASSIGN:   return "token_assign";
+	case TOKEN_PLUS:     return "token_plus";
+	case TOKEN_MINUS:    return "token_minus";
+	case TOKEN_ASTERISK: return "token_asterisk";
+	case TOKEN_SLASH:    return "token_slash";
+	case TOKEN_CARET:    return "token_caret";
+	case TOKEN_TILDE:    return "token_tilde";
+	case TOKEN_BANG:     return "token_bang";
+	case TOKEN_QUESTION: return "token_question";
+	case TOKEN_COLON:    return "token_colon";
+	case TOKEN_NAME:     return "token_name";
+	case TOKEN_EOF:      return "token_eof";
+	default:             return "token_invalid";
+	}
+}
+
+enum TokenType
+TokenType_punctuator(char c)
+{
+	switch (c)
+	{
+	case '(': return TOKEN_LPAREN;
+	case ')': return TOKEN_RPAREN;
+	case ',': return TOKEN_COMMA;
+	case '=': return TOKEN_ASSIGN;
+	case '+': return TOKEN_PLUS;
+	case '-': return TOKEN_MINUS;
+	case '*': return TOKEN_ASTERISK;
+	case '/': return TOKEN_SLASH;
+	case '^': return TOKEN_CARET;
+	case '~': return TOKEN_TILDE;
+	case '!': return TOKEN_BANG;
+	case '?': return TOKEN_QUESTION;
+	case ':': return TOKEN_COLON;
+	default:  return TOKEN_INVALID;
+	}
+}
--- src/token.h
+++ src/token.h
@@ -5,6 +5,61 @@
 extern "C" {
 #endif
 
+// the type of token that a Token can represent
+enum TokenType
+{
+	TOKEN_LPAREN,
+	TOKEN_RPAREN,
+	TOKEN_COMMA,
+	TOKEN_ASSIGN,
+	TOKEN_PLUS,
+	TOKEN_MINUS,
+	TOKEN_ASTERISK,
+	TOKEN_SLASH,
+	TOKEN_CARET,
+	TOKEN_TILDE,
+	TOKEN_BANG,
+	TOKEN_QUESTION,
+	TOKEN_COLON,
+	TOKEN_NAME,
+	TOKEN_EOF,
+	TOKEN_INVALID
+};
+
+// represents a single immutable lexer token
+// use the provided functions instead of the struct members
+struct Token
+{
+	enum TokenType type;
+	const char* begin;
+	const char* end;
+};
+
+// initializes a Token
+void
+Token_init(struct Token* t, enum TokenType type, const char* begin, const char* end);
+
+// getter for type
+enum TokenType
+Token_getType(struct Token* t);
+
+// getter for begin
+const char*
+Token_getBegin(struct Token* t);
+
+// getter for end
+const char*
+Token_getEnd(struct Token* t);
+
+// converts a TokenType to a string
+const char*
+TokenType_toString(enum TokenType type);
+
+// converts a punctuation character into a TokenType
+// includes symbols and operators but not names
+enum TokenType
+TokenType_punctuator(char c);
+
 #ifdef __cplusplus
 }
 #endif
