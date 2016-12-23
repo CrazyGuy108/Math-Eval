4b76ab276cb54268bdbe01a5f54fa9f8096cab8b
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-22 16:53:46 -0800

    Create Lexer class
    This class is meant to take in a source string and split it up into
    Tokens by stepping through said string.
    
    The Lexer_next() function is the main one of interest. When called, the lexer splits up the source string into Tokens to be fed into the parser.

--- src/lexer.c
+++ src/lexer.c
@@ -1,1 +1,46 @@
 #include "lexer.h"
+
+void
+Lexer_init(struct Lexer* l, const char* source)
+{
+	l->pos = source;
+}
+
+struct Token
+Lexer_next(struct Lexer* l)
+{
+	struct Token t;
+	char c;
+
+	do
+	{
+		c = *l->pos++;
+		enum TokenType type = TokenType_punctuator(c);
+
+		if (type == TOKEN_INVALID)
+		{
+			if (isalpha(c))
+			{
+				const char* begin = l->pos - 1;
+
+				while (isalpha(*l->pos))
+				{
+					++l->pos;
+				}
+
+				Token_init(&t, TOKEN_NAME, begin, l->pos);
+				return t;
+			}
+			
+		}
+		else
+		{
+			Token_init(&t, type, NULL, NULL);
+			return t;
+		}
+	}
+	while (c != '\0');
+
+	Token_init(&t, TOKEN_EOF, NULL, NULL);
+	return t;
+}
--- src/lexer.h
+++ src/lexer.h
@@ -5,6 +5,24 @@
 extern "C" {
 #endif
 
+#include <ctype.h>
+#include <stdlib.h>
+#include "token.h"
+
+struct Lexer
+{
+	const char* pos;
+};
+
+// initializes a Lexer
+void
+Lexer_init(struct Lexer* l, const char* source);
+
+// gets/generates the next Token from the source string
+// if at the end of the string, the lexer creates an EOF token
+struct Token
+Lexer_next(struct Lexer* l);
+
 #ifdef __cplusplus
 }
 #endif
