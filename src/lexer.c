d27ddccde8e669bbc8a9a05fb10c4c567d53b7d8
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-22 20:22:00 -0800

    Add name check to match()
    The match() function wasn't entirely complete, since it didn't match a name against the lexer rules. It's perfectly reasonable to be able to do so since the lexer rules are so simple that you don't need any lookahead.
    
    This also allowed me to refactor the Lexer_next() algorithm to remove an extra brace nest.

--- src/lexer.c
+++ src/lexer.c
@@ -21,23 +21,20 @@ Lexer_next(struct Lexer* l)
 		c = *l->pos++;
 		enum TokenType type = match(c);
 
-		if (type == TOKEN_INVALID)
+		if (type == TOKEN_NAME && isalpha(c))
 		{
-			if (isalpha(c))
+			const char* begin = l->pos - 1;
+
+			while (isalpha(*l->pos))
 			{
-				const char* begin = l->pos - 1;
-
-				while (isalpha(*l->pos))
-				{
-					++l->pos;
-				}
-
-				Token_init(&t, TOKEN_NAME, begin, l->pos);
-				return t;
+				++l->pos;
 			}
+
+			Token_init(&t, TOKEN_NAME, begin, l->pos);
+			return t;
 			
 		}
-		else
+		else if (type != TOKEN_INVALID)
 		{
 			Token_init(&t, type, NULL, NULL);
 			return t;
@@ -54,19 +51,24 @@ match(char c)
 {
 	switch (c)
 	{
-	case '(': return TOKEN_LPAREN;
-	case ')': return TOKEN_RPAREN;
-	case ',': return TOKEN_COMMA;
-	case '=': return TOKEN_ASSIGN;
-	case '+': return TOKEN_PLUS;
-	case '-': return TOKEN_MINUS;
-	case '*': return TOKEN_ASTERISK;
-	case '/': return TOKEN_SLASH;
-	case '^': return TOKEN_CARET;
-	case '~': return TOKEN_TILDE;
-	case '!': return TOKEN_BANG;
-	case '?': return TOKEN_QUESTION;
-	case ':': return TOKEN_COLON;
-	default:  return TOKEN_INVALID;
+	case '(':  return TOKEN_LPAREN;
+	case ')':  return TOKEN_RPAREN;
+	case ',':  return TOKEN_COMMA;
+	case '=':  return TOKEN_ASSIGN;
+	case '+':  return TOKEN_PLUS;
+	case '-':  return TOKEN_MINUS;
+	case '*':  return TOKEN_ASTERISK;
+	case '/':  return TOKEN_SLASH;
+	case '^':  return TOKEN_CARET;
+	case '~':  return TOKEN_TILDE;
+	case '!':  return TOKEN_BANG;
+	case '?':  return TOKEN_QUESTION;
+	case ':':  return TOKEN_COLON;
+	case '\0': return TOKEN_EOF;
+	default:
+		if (isalpha(c))
+			return TOKEN_NAME;
+		else
+			return TOKEN_INVALID;
 	}
 }
