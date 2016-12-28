9ac2323abb66e486a959a534a3fd60d9343ad73b
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-28 11:40:43 -0800

    Remove unnecessary TokenTypes
    I decided to restrict the parser to basic PEMDAS infix expressions and prefix expressions in order to simplify the parser a bit more.

--- src/lexer.c
+++ src/lexer.c
@@ -53,17 +53,11 @@ match(char c)
 	{
 	case '(':  return TOKEN_LPAREN;
 	case ')':  return TOKEN_RPAREN;
-	case ',':  return TOKEN_COMMA;
-	case '=':  return TOKEN_ASSIGN;
 	case '+':  return TOKEN_PLUS;
 	case '-':  return TOKEN_MINUS;
 	case '*':  return TOKEN_ASTERISK;
 	case '/':  return TOKEN_SLASH;
 	case '^':  return TOKEN_CARET;
-	case '~':  return TOKEN_TILDE;
-	case '!':  return TOKEN_BANG;
-	case '?':  return TOKEN_QUESTION;
-	case ':':  return TOKEN_COLON;
 	case '\0': return TOKEN_EOF;
 	default:
 		if (isalpha(c))
--- src/token.c
+++ src/token.c
@@ -33,17 +33,11 @@ TokenType_toString(enum TokenType type)
 	{
 	case TOKEN_LPAREN:   return "token_lparen";
 	case TOKEN_RPAREN:   return "token_rparen";
-	case TOKEN_COMMA:    return "token_comma";
-	case TOKEN_ASSIGN:   return "token_assign";
 	case TOKEN_PLUS:     return "token_plus";
 	case TOKEN_MINUS:    return "token_minus";
 	case TOKEN_ASTERISK: return "token_asterisk";
 	case TOKEN_SLASH:    return "token_slash";
 	case TOKEN_CARET:    return "token_caret";
-	case TOKEN_TILDE:    return "token_tilde";
-	case TOKEN_BANG:     return "token_bang";
-	case TOKEN_QUESTION: return "token_question";
-	case TOKEN_COLON:    return "token_colon";
 	case TOKEN_NAME:     return "token_name";
 	case TOKEN_EOF:      return "token_eof";
 	default:             return "token_invalid";
