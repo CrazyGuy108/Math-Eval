8ec0225c25e21b24a2e95b4c29946399ab97e963
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-29 12:43:59 -0800

    Add prefix parselets
    The PrefixParselet abstract base class is a type of parselet built for parsing
    things like unary operators, parenthesis groups, and integers. It uses a
    virtual call to its parse() function, taking the Parser used and the Token
    that caused the Parser to call that particular parselet.
    
    The parse() functions currently have a placeholder returning NULL. They're just
    here to provide a useful interface to the Parser before the parselets are
    actually implemented.
    
    There are 3 PrefixParselet subclasses:
    
    The GroupParselet parses an expression grouped within parentheses, e.g.
    "(a+b)". A placeholder was put inside its overriden v_parse() function for now.
    
    The IntParselet is for parsing integer expressions, i.e. a single integer
    token. It really doesn't do much else here but it's there for parsing purposes.
    
    The UnaryParselet is for parsing unary operators like "-a". Since operators
    have precedence, it also has a member variable called precedence.

--- src/parselet.c
+++ src/parselet.c
@@ -1,1 +1,145 @@
 #include "parselet.h"
+
+// convenient typedefs for really long function pointers
+typedef void (*PrefixParselet_dtor_t)(struct PrefixParselet*);
+typedef struct Expr* (*PrefixParselet_parse_t)(struct PrefixParselet*,
+	struct Parser*, const struct Token*);
+
+// main vtable for PrefixParselet subclasses
+struct PrefixParselet_vtable
+{
+	PrefixParselet_dtor_t dtor;
+	PrefixParselet_parse_t parse;
+};
+
+// protected virtual functions
+static void
+PrefixParselet_v_dtor(struct PrefixParselet* this);
+
+static void
+GroupParselet_v_dtor(struct GroupParselet* this);
+
+static void
+IntParselet_v_dtor(struct IntParselet* this);
+
+static void
+UnaryParselet_v_dtor(struct UnaryParselet* this);
+
+static struct Expr*
+GroupParselet_v_parse(struct GroupParselet* this, struct Parser* parser,
+	const struct Token* token);
+
+static struct Expr*
+IntParselet_v_parse(struct IntParselet* this, struct Parser* parser,
+	const struct Token* token);
+
+static struct Expr*
+UnaryParselet_v_parse(struct UnaryParselet* this, struct Parser* parser,
+	const struct Token* token);
+
+// vtables
+const struct PrefixParselet_vtable PrefixParselet_vtable = { NULL, NULL };
+const struct PrefixParselet_vtable GroupParselet_vtable =
+{
+	(PrefixParselet_dtor_t)&GroupParselet_v_dtor,
+	(PrefixParselet_parse_t)&GroupParselet_v_parse
+};
+const struct PrefixParselet_vtable IntParselet_vtable =
+{
+	(PrefixParselet_dtor_t)&IntParselet_v_dtor,
+	(PrefixParselet_parse_t)&IntParselet_v_parse
+};
+const struct PrefixParselet_vtable UnaryParselet_vtable =
+{
+	(PrefixParselet_dtor_t)&UnaryParselet_v_dtor,
+	(PrefixParselet_parse_t)&UnaryParselet_v_parse
+};
+
+// public functions
+
+void
+PrefixParselet_init(struct PrefixParselet* this)
+{
+	this->vtable = &PrefixParselet_vtable;
+}
+
+void
+PrefixParselet_dtor(struct PrefixParselet* this)
+{
+	this->vtable->dtor(this);
+}
+
+struct Expr*
+PrefixParselet_parse(struct PrefixParselet* this, struct Parser* parser,
+	const struct Token* token)
+{
+	return this->vtable->parse(this, parser, token);
+}
+
+void
+GroupParselet_init(struct GroupParselet* this)
+{
+	PrefixParselet_init(&this->super);
+	this->super.vtable = &GroupParselet_vtable;
+}
+
+void
+IntParselet_init(struct IntParselet* this)
+{
+	PrefixParselet_init(&this->super);
+	this->super.vtable = &IntParselet_vtable;
+}
+
+void
+UnaryParselet_init(struct UnaryParselet* this, int precedence)
+{
+	PrefixParselet_init(&this->super);
+	this->super.vtable = &UnaryParselet_vtable;
+	this->precedence = precedence;
+}
+
+// protected virtual functions
+
+void
+PrefixParselet_v_dtor(struct PrefixParselet* this)
+{
+}
+
+void
+GroupParselet_v_dtor(struct GroupParselet* this)
+{
+	PrefixParselet_v_dtor(&this->super);
+}
+
+void
+IntParselet_v_dtor(struct IntParselet* this)
+{
+	PrefixParselet_v_dtor(&this->super);
+}
+
+void
+UnaryParselet_v_dtor(struct UnaryParselet* this)
+{
+	PrefixParselet_v_dtor(&this->super);
+}
+
+static struct Expr*
+GroupParselet_v_parse(struct GroupParselet* this, struct Parser* parser,
+	const struct Token* token)
+{
+	return NULL; // placeholder
+}
+
+static struct Expr*
+IntParselet_v_parse(struct IntParselet* this, struct Parser* parser,
+	const struct Token* token)
+{
+	return NULL; // placeholder
+}
+
+static struct Expr*
+UnaryParselet_v_parse(struct UnaryParselet* this, struct Parser* parser,
+	const struct Token* token)
+{
+	return NULL; // placeholder
+}
--- src/parselet.h
+++ src/parselet.h
@@ -5,6 +5,61 @@
 extern "C" {
 #endif
 
+#include <stdlib.h>
+#include "parser.h"
+#include "token.h"
+
+// base class for prefix parselets
+struct PrefixParselet
+{
+	const struct PrefixParselet_vtable* vtable;
+};
+
+// parselet for expressions grouped inside parentheses
+struct GroupParselet
+{
+	struct PrefixParselet super;
+};
+
+// parselet for integers
+struct IntParselet
+{
+	struct PrefixParselet super;
+};
+
+// parselet for unary operators
+struct UnaryParselet
+{
+	struct PrefixParselet super;
+	int precedence;
+};
+
+// PrefixParselet constructor
+void
+PrefixParselet_init(struct PrefixParselet* this);
+
+// virtual destructor wrapper for PrefixParselet subclasses
+void
+PrefixParselet_dtor(struct PrefixParselet* this);
+
+// virtual function wrapper for PrefixParselet subclasses
+// generates an expression using the parser and the current token
+struct Expr*
+PrefixParselet_parse(struct PrefixParselet* this, struct Parser* parser,
+	const struct Token* token);
+
+// GroupParselet constructor
+void
+GroupParselet_init(struct GroupParselet* this);
+
+// IntParselet constructor
+void
+IntParselet_init(struct IntParselet* this);
+
+// UnaryParselet constructor
+void
+UnaryParselet_init(struct UnaryParselet* this, int precedence);
+
 #ifdef __cplusplus
 }
 #endif
