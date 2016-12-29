92ffa93f720755cca0aebb0497155a6a2a48cafc
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-28 21:51:26 -0800

    Create Parser class
    This parser is only partially implemented, since it relies on a class
    heirarchy to do the actual parsing for it. These will be known as "parselets".
    The purpose of this commit is to initially setup an interface for the
    "parselets" to use, with everything but Parser_parseExpr() implemented.

--- src/parser.c
+++ src/parser.c
@@ -1,1 +1,30 @@
 #include "parser.h"
+
+void
+Parser_init(struct Parser* this, struct Lexer* lexer)
+{
+	this->lexer = lexer;
+}
+
+struct Expr*
+Parser_parseExpr(struct Parser* this, size_t precedence)
+{
+	return NULL; // placeholder
+}
+
+void
+Parser_freeExpr(struct Expr** expr)
+{
+	if (expr != NULL && *expr != NULL)
+	{
+		Expr_dtor(*expr);
+		free(*expr);
+		*expr = NULL;
+	}
+}
+
+struct Token
+Parser_consume(struct Parser* this)
+{
+	return Lexer_next(this->lexer);
+}
--- src/parser.h
+++ src/parser.h
@@ -5,6 +5,31 @@
 extern "C" {
 #endif
 
+#include <stdlib.h>
+#include "expr.h"
+#include "lexer.h"
+
+struct Parser
+{
+	struct Lexer* lexer;
+};
+
+// Parser constructor
+void
+Parser_init(struct Parser* this, struct Lexer* lexer);
+
+// generates an expression of a precedence not lower than what was given
+struct Expr*
+Parser_parseExpr(struct Parser* this, size_t precedence);
+
+// destroys an expression created by Parser_parseExpr
+void
+Parser_freeExpr(struct Expr** expr);
+
+// consumes the next token
+struct Token
+Parser_consume(struct Parser* this);
+
 #ifdef __cplusplus
 }
 #endif
