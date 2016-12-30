0ec05f06f4e099954b8cda73e046bebcf3a37643
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-30 14:58:58 -0800

    Remove 'token_' prefix

--- src/token.c
+++ src/token.c
@@ -24,15 +24,15 @@ TokenType_toString(enum TokenType type)
 {
 	switch (type)
 	{
-	case TOKEN_LPAREN:   return "token_lparen";
-	case TOKEN_RPAREN:   return "token_rparen";
-	case TOKEN_PLUS:     return "token_plus";
-	case TOKEN_MINUS:    return "token_minus";
-	case TOKEN_ASTERISK: return "token_asterisk";
-	case TOKEN_SLASH:    return "token_slash";
-	case TOKEN_CARET:    return "token_caret";
-	case TOKEN_INTEGER:  return "token_integer";
-	case TOKEN_EOF:      return "token_eof";
-	default:             return "token_invalid";
+	case TOKEN_LPAREN:   return "lparen";
+	case TOKEN_RPAREN:   return "rparen";
+	case TOKEN_PLUS:     return "plus";
+	case TOKEN_MINUS:    return "minus";
+	case TOKEN_ASTERISK: return "asterisk";
+	case TOKEN_SLASH:    return "slash";
+	case TOKEN_CARET:    return "caret";
+	case TOKEN_INTEGER:  return "integer";
+	case TOKEN_EOF:      return "eof";
+	default:             return "invalid";
 	}
 }
