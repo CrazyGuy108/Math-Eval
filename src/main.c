e146cd3ea09a39c55bfe7c8cb9fe54c7fb16d048
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-31 14:05:22 -0800

    Add eval loop
    The main program works kind of like a command line. You enter in an expression
    (for now, less than 256 characters), then the result is printed out. The same
    thing is done infinitely until you give it a blank input.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "mathparser.h"
+
 int
 main()
 {
+	char input[256];
+	while (1)
+	{
+		fputs(">", stdout);
+		fgets(input, sizeof(input), stdin);
+		if (input[0] == '\n')
+		{
+			break;
+		}
+		fseek(stdin, 0, SEEK_END);
 
+		struct Lexer lexer;
+		struct MathParser parser;
+		Lexer_init(&lexer, input);
+		MathParser_init(&parser, &lexer);
+		struct Expr* expr =
+			Parser_parseExpr((struct Parser*)&parser, 0);
+		printf("%d\n", Expr_eval(expr));
+		Parser_freeExpr(&expr);
+	}
 }
