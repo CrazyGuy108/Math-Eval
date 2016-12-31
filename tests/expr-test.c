4b986e9d3a0d90b7a0e8232822eb944959ef9c80
CrazyGuy108 <taylor@hansenfamily.us>
2016-12-30 17:36:55 -0800

    Add Expr test
    The test consists of building an expression tree equivalent to "10 + (-9)".

--- tests/expr-test.c
+++ tests/expr-test.c
@@ -5,15 +5,20 @@ int
 main()
 {
 	// same as the expression "10 + (-9)"
-	struct IntExpr expr1;
-	struct IntExpr expr2;
-	struct UnaryExpr expr3;
-	struct BinaryExpr expr;
-	IntExpr_init(&expr1, 9);
-	IntExpr_init(&expr2, 10);
-	UnaryExpr_init(&expr3, TOKEN_MINUS, (struct Expr*)&expr1);
-	BinaryExpr_init(&expr, (struct Expr*)&expr2, TOKEN_PLUS,
-		(struct Expr*)&expr3);
-	Expr_fprint((struct Expr*)&expr, stdout);
-	Expr_dtor((struct Expr*)&expr);
+	struct IntExpr* expr1 = malloc(sizeof(struct IntExpr));
+	struct IntExpr* expr2 = malloc(sizeof(struct IntExpr));
+	IntExpr_init(expr1, 10);
+	IntExpr_init(expr2, 9);
+
+	struct UnaryExpr* expr3 = malloc(sizeof(struct UnaryExpr));
+	UnaryExpr_init(expr3, TOKEN_MINUS, (struct Expr*)expr2);
+
+	struct BinaryExpr* expr = malloc(sizeof(struct BinaryExpr));
+	BinaryExpr_init(expr, (struct Expr*)expr1, TOKEN_PLUS,
+		(struct Expr*)expr3);
+
+	Expr_fprint((struct Expr*)expr, stdout);
+
+	Expr_dtor((struct Expr*)expr);
+	free(expr);
 }
